19a6b88bb477f74cb19ec81bb5aab95524474461
ferchach_iliass <iliass.ferchach@gmail.com>
2024-04-06 16:26:13 +0200

    PictureComponent + PhototequeComponent + premiere vue

--- frontend/myApp/src/app/app.module.ts
+++ frontend/myApp/src/app/app.module.ts
@@ -3,16 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PictureComponent } from './picture/picture.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { PhototequeComponent } from './phototeque/phototeque.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, PhototequeComponent, PictureComponent],
+  imports: [BrowserModule, AppRoutingModule, MatIconModule, MatButtonModule],
+  providers: [provideAnimationsAsync()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ frontend/myApp/src/app/picture/picture.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-picture',
+  templateUrl: './picture.component.html',
+  styleUrls: ['./picture.component.css'],
+})
+export class PictureComponent {
+  @Input() url: string = '';
+
+  constructor() {}
+}
